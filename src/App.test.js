e9728b724c2748c5704a409e4053347d9b74c441
Ludmila Kellerová <dvojitywendy@seznam.cz>
2019-05-25 22:13:02 +0200

    Mufka00001 - First changes to Wendy in main page and new tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+xit('renders wrapper without crashing', () => {
+  const div = document.createElement('div');
+  const wrapper = ReactDOM.render(<App />, div);
+   const welcome = <h2>Welcome to React</h2>;
+   ReactDOM.render(wrapper, welcome);
+     expect(wrapper.contains(welcome)).toBe(true);
+  // expect(wrapper.contains(welcome)).toEqual(true);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders welcome message', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Wendy')).toBeInTheDocument();
 });
